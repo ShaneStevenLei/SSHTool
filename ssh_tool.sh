0e961e02350732fe02f0b3261055fbd3b5743e52
ShaneStevenLei <ShaneStevenLei@outlook.com>
2016-07-25 10:47:39 +0800

    使用配置文件来配置参数
    Signed-off-by: ShaneStevenLei <ShaneStevenLei@outlook.com>

--- ssh_tool.sh
+++ ssh_tool.sh
@@ -1,9 +1,57 @@
 #!/bin/bash
 
-key=''
-user=''
-port=''
-host=''
+exe_path=$(cd "$(dirname "$0")"; pwd)
+
+if [[ ! -f ${exe_path}/ssh_tool.config ]]
+then
+	read -p "配置文件${exe_path}/ssh_tool.config不存在,是否创建(Y/N)" -s create_config
+	case ${create_config} in
+		'Y'|'y')
+			echo 'key=' >> ${exe_path}/ssh_tool.config
+			echo 'user=' >> ${exe_path}/ssh_tool.config
+			echo 'port=' >> ${exe_path}/ssh_tool.config
+			echo 'host=' >> ${exe_path}/ssh_tool.config
+			echo '创建完成，请修改配置文件${exe_path}/ssh_tool.config'
+			exit
+			;;
+		'N'|'n')
+			exit
+			;;
+		*)
+			exit
+			;;
+	esac
+fi
+
+key=`cat ${exe_path}/ssh_tool.config | grep 'key' | awk -F '=' '{print $2}' | head -n 1`
+user=`cat ${exe_path}/ssh_tool.config | grep 'user' | awk -F '=' '{print $2}' | head -n 1`
+port=`cat ${exe_path}/ssh_tool.config | grep 'port' | awk -F '=' '{print $2}' | head -n 1`
+host=`cat ${exe_path}/ssh_tool.config | grep 'host' | awk -F '=' '{print $2}' | head -n 1`
+
+if [[ $key == '' ]]
+then
+	echo '请在配置文件中设置key'
+	exit
+fi
+
+if [[ $user == '' ]]
+then
+	echo '请在配置文件中设置user'
+	exit
+fi
+
+if [[ $port == '' ]]
+then
+	echo '请在配置文件中设置port'
+	exit
+fi
+
+if [[ $host == '' ]]
+then
+	echo '请在配置文件中设置host'
+	exit
+fi
+
 
 if [[ `which ssh` == '' ]]
 then
@@ -27,4 +75,4 @@ then
 fi
 
 v_code=`oathtool --totp -b -d 6 $key`
-./ssh_tool.expect $user $port $host $v_code
+expect ${exe_path}/ssh_tool.expect $user $port $host $v_code
